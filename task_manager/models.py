5c2e3ec096263107ae2a06f431c2f235a38eb76f
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-06-23 17:09:49 +0300

    implemented models and registration user model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+
+    class Priorety(models.TextChoices):
+        L = "L", "Low"
+        M = "M", "Medium"
+        H = "H", "High"
+        C = "C", "Critical"
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=False)
+    deadline = models.DateField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(choices=Priorety.choices, default=Priorety.L, max_length=1)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
